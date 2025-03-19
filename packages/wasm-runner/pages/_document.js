b8917179ead2931194ad3c1b73273721a8e1c44a
Ankesh Bharti <hey@ankeshbharti.com>
2025-03-19 13:46:04 +0530

    Refactor MyApp component to remove Vercel Analytics and create a new custom Document component for manual analytics integration

---
+++ packages/wasm-runner/pages/_document.js
@@ -0,0 +1,19 @@
+import { Html, Head, Main, NextScript } from 'next/document';
+
+export default function Document() {
+  return (
+    <Html>
+      <Head>
+        {/* Vercel Analytics Script - Added manually to avoid React hooks issues */}
+        <script
+          defer
+          src="/_vercel/insights/script.js"
+        ></script>
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+} 
