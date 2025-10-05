aa023f9789ad0b6c49a7274f80c93be8ca3002cb
madclaws <pavanan.anandu63@gmail.com>
2025-10-05 16:23:11 +0530

    chore: sample in main

--- src/main.rs
+++ src/main.rs
@@ -4,8 +4,11 @@ use tilekit::modelfile;
 pub fn main() -> Result<(), Box<dyn Error>> {
     println!("{}:{}", env!("CARGO_PKG_NAME"), env!("CARGO_PKG_VERSION"));
 
-    let modf = modelfile::parse_from_file("assets/tests/mistral.modelfile");
-
-    println!("{:?}", modf);
+    let mut modf = modelfile::parse_from_file("fixtures/a.modelfile")?;
+    modf.add_parameter("temperature", "0.5")?;
+    modf.add_message("user", "Is Rust a functional language")?;
+    modf.add_message("assistant", "no")?;
+    modf.build()?;
+    println!("{:?}", modf.to_string());
     Ok(())
 }
